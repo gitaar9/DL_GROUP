76d2067f1700e1a9fbe0a4f8d19304bac95a9a45
gitaar9 <thijs.eker@gmail.com>
2019-05-03 23:19:49 +0200

    refactored the converter

--- converter.py
+++ converter.py
@@ -28,9 +28,7 @@ class Converter:
     def load_midi_file(self, folder_path, filename):
         mf = midi.MidiFile()
         mf.open(os.path.join(folder_path, filename), 'rb')
-
-        # read in the midi file
-        mf.read()
+        mf.read()  # read in the midi file
         mf.close()
         return mf
 
@@ -46,13 +44,21 @@ class Converter:
                 self.fill_note_in_array(array, note.offset, note.duration.quarterLength, note.pitch.octave, note.pitch.pitchClass)
         return array
 
-    @staticmethod
-    def midi_files_in_dir(path):
-        return [f for f in os.listdir(path) if f.endswith(".mid") or f.endswith(".MID")]
+    def slice_song(self, array_2d):
+        sliced_array = None
+        song_length = array_2d.shape[1]
+        for start_of_slice_idx in range(0, song_length, self.slice_size):
+            end_of_slice_idx = start_of_slice_idx + self.slice_size
+            if end_of_slice_idx > song_length:
+                break
+            song_slice = np.array([array_2d[:, start_of_slice_idx:end_of_slice_idx]])
+            sliced_array = np.concatenate([sliced_array, song_slice], axis=0) if sliced_array is not None else song_slice
+
+        return sliced_array
 
     def folder_to_3d_array(self, folder_name):
         folder_path = os.path.join(self.base_path, folder_name)
-        filenames = self.midi_files_in_dir(path=folder_path)
+        filenames = [f for f in os.listdir(folder_path) if f.endswith(".mid") or f.endswith(".MID")]
         print("Found {} midi files for {}".format(len(filenames), folder_name))
 
         song_idxs = []
@@ -63,39 +69,35 @@ class Converter:
             except midi.MidiException:
                 print('Skipping {} due to midi exception'.format(filename))
                 continue
+
             song_stream = midi.translate.midiFileToStream(midi_file)
             song_2d_array = self.stream_to_2d_array(song_stream)
 
             song_idxs.append(folder_array.shape[0] if folder_array is not None else 0)  # So we know here songs start
 
-            # Slice up the array so we can stack all songs together in one 3d array
-            song_length = song_2d_array.shape[1]
-            for start_of_slice_idx in range(0, song_length, self.slice_size):
-                end_of_slice_idx = start_of_slice_idx + self.slice_size
-                if end_of_slice_idx > song_length:
-                    break
-                array_slice = song_2d_array[:, start_of_slice_idx:end_of_slice_idx]
-                if folder_array is None:
-                    folder_array = np.array([array_slice])
-                else:
-                    folder_array = np.concatenate([folder_array, np.array([array_slice])], axis=0)
+            sliced_song_array = self.slice_song(song_2d_array)
+            folder_array = np.concatenate([folder_array, sliced_song_array]) if folder_array is not None else sliced_song_array
 
         return folder_array, np.array(song_idxs)
 
 
 if __name__ == '__main__':
-    d = "./data/midi-classic-music/"
-    folders = [o for o in os.listdir(d) if os.path.isdir(os.path.join(d, o))]
+    midi_directory = "./data/midi-classic-music/"
+    folders = [o for o in os.listdir(midi_directory) if os.path.isdir(os.path.join(midi_directory, o))]
     con = Converter()
     for folder_name in folders:
         try:
-            np.load(os.path.join(d, folder_name, "{}_song_idxs.npy".format(folder_name)))
+            np.load(os.path.join(midi_directory, folder_name, "{}_song_idxs.npy".format(folder_name)))
             print("npy file already exists for {}".format(folder_name))
             continue
         except FileNotFoundError:
             pass
+
+        # parse the midi files to a nparray
         array_3d, song_idxs = con.folder_to_3d_array(folder_name)
-        folder_path = os.path.join(d, folder_name)
+
+        # save the parsed midi as a .npy file
+        folder_path = os.path.join(midi_directory, folder_name)
         np.save(os.path.join(folder_path, "{}_data".format(folder_name)), array_3d)
         np.save(os.path.join(folder_path, "{}_song_idxs".format(folder_name)), song_idxs)
         print("3D Array shape: {}\n{} song indexes: {}".format(array_3d.shape, len(song_idxs), song_idxs))
