6d8a0aab3f115c5d2d55a2de5eefdfa1d55841dd
gitaar9 <thijs.eker@gmail.com>
2019-05-05 01:10:59 +0200

    another change

--- stupid_overwrites.py
+++ stupid_overwrites.py
@@ -3,7 +3,6 @@ from collections import OrderedDict
 
 import torch.nn as nn
 import torch.utils.model_zoo as model_zoo
-from torch._thnn import backend as thnn_backend
 from torchvision.models.densenet import _DenseBlock, _Transition, DenseNet as BaseDenseNet, \
     model_urls as dense_model_urls
 from torchvision.models.resnet import BasicBlock, model_urls as res_model_urls, ResNet as BaseResNet
@@ -24,55 +23,11 @@ class DenseNet(BaseDenseNet):
         num_classes (int) - number of classification classes
     """
 
-    def __init__(self, growth_rate=32, block_config=(6, 12, 24, 16),
-                 num_init_features=64, bn_size=4, drop_rate=0, num_classes=1000):
-        # replace call to init of module
-        self._backend = thnn_backend
-        self._parameters = OrderedDict()
-        self._buffers = OrderedDict()
-        self._backward_hooks = OrderedDict()
-        self._forward_hooks = OrderedDict()
-        self._forward_pre_hooks = OrderedDict()
-        self._state_dict_hooks = OrderedDict()
-        self._load_state_dict_pre_hooks = OrderedDict()
-        self._modules = OrderedDict()
-        self.training = True
+    def __init__(self, *args, **kwargs):
 
-        # First convolution
-        self.features = nn.Sequential(OrderedDict([
-            ('conv0', nn.Conv2d(1, num_init_features, kernel_size=7, stride=2, padding=3, bias=False)),
-            ('norm0', nn.BatchNorm2d(num_init_features)),
-            ('relu0', nn.ReLU(inplace=True)),
-            ('pool0', nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
-        ]))
+        super(DenseNet, self).__init__(*args, **kwargs)
 
-        # Each denseblock
-        num_features = num_init_features
-        for i, num_layers in enumerate(block_config):
-            block = _DenseBlock(num_layers=num_layers, num_input_features=num_features,
-                                bn_size=bn_size, growth_rate=growth_rate, drop_rate=drop_rate)
-            self.features.add_module('denseblock%d' % (i + 1), block)
-            num_features = num_features + num_layers * growth_rate
-            if i != len(block_config) - 1:
-                trans = _Transition(num_input_features=num_features, num_output_features=num_features // 2)
-                self.features.add_module('transition%d' % (i + 1), trans)
-                num_features = num_features // 2
-
-        # Final batch norm
-        self.features.add_module('norm5', nn.BatchNorm2d(num_features))
-
-        # Linear layer
-        self.classifier = nn.Linear(num_features, num_classes)
-
-        # Official init from torch repo.
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight)
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Linear):
-                nn.init.constant_(m.bias, 0)
+        print(self.features[0])
 
 
 def densenet121(pretrained=False, **kwargs):
