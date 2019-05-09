47943ed00c436e55276bbaed81b9fdeeb81ee0c8
gitaar9 <thijs.eker@gmail.com>
2019-05-09 16:44:55 +0200

    basic densenet

--- stupid_overwrites.py
+++ stupid_overwrites.py
@@ -1,31 +1,74 @@
 import re
+from collections import OrderedDict
 
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.utils.model_zoo as model_zoo
-from torchvision.models.densenet import model_urls as dense_model_urls, DenseNet
-from torchvision.models.resnet import BasicBlock, model_urls as res_model_urls, ResNet
+from torchvision.models.densenet import model_urls as dense_model_urls, _DenseBlock, _Transition
 
 
 # CHANGE DENSENET TO USE A SINGLE CHANNEL INPUT LAYER #
-# class DenseNet(BaseDenseNet):
-#     r"""Densenet-BC model class, based on
-#     `"Densely Connected Convolutional Networks" <https://arxiv.org/pdf/1608.06993.pdf>`_
-#
-#     Args:
-#         growth_rate (int) - how many filters to add each layer (`k` in paper)
-#         block_config (list of 4 ints) - how many layers in each pooling block
-#         num_init_features (int) - the number of filters to learn in the first convolution layer
-#         bn_size (int) - multiplicative factor for number of bottle neck layers
-#           (i.e. bn_size * k features in the bottleneck layer)
-#         drop_rate (float) - dropout rate after each dense layer
-#         num_classes (int) - number of classification classes
-#     """
-#
-#     def __init__(self, *args, **kwargs):
-#
-#         super(DenseNet, self).__init__(*args, **kwargs)
-#         self.features[0] = nn.Conv2d(1, self.features[0].out_channels, kernel_size=7, stride=2, padding=3, bias=False)
-#         print(self.features[0])
+class DenseNet(nn.Module):
+    r"""Densenet-BC model class, based on
+    `"Densely Connected Convolutional Networks" <https://arxiv.org/pdf/1608.06993.pdf>`_
+
+    Args:
+        growth_rate (int) - how many filters to add each layer (`k` in paper)
+        block_config (list of 4 ints) - how many layers in each pooling block
+        num_init_features (int) - the number of filters to learn in the first convolution layer
+        bn_size (int) - multiplicative factor for number of bottle neck layers
+          (i.e. bn_size * k features in the bottleneck layer)
+        drop_rate (float) - dropout rate after each dense layer
+        num_classes (int) - number of classification classes
+    """
+
+    def __init__(self, growth_rate=32, block_config=(6, 12, 24, 16),
+                 num_init_features=64, bn_size=4, drop_rate=0, num_classes=1000):
+
+        super(DenseNet, self).__init__()
+
+        # First convolution
+        self.features = nn.Sequential(OrderedDict([
+            ('conv0', nn.Conv2d(3, num_init_features, kernel_size=7, stride=2, padding=3, bias=False)),
+            ('norm0', nn.BatchNorm2d(num_init_features)),
+            ('relu0', nn.ReLU(inplace=True)),
+            ('pool0', nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
+        ]))
+
+        # Each denseblock
+        num_features = num_init_features
+        for i, num_layers in enumerate(block_config):
+            block = _DenseBlock(num_layers=num_layers, num_input_features=num_features,
+                                bn_size=bn_size, growth_rate=growth_rate, drop_rate=drop_rate)
+            self.features.add_module('denseblock%d' % (i + 1), block)
+            num_features = num_features + num_layers * growth_rate
+            if i != len(block_config) - 1:
+                trans = _Transition(num_input_features=num_features, num_output_features=num_features // 2)
+                self.features.add_module('transition%d' % (i + 1), trans)
+                num_features = num_features // 2
+
+        # Final batch norm
+        self.features.add_module('norm5', nn.BatchNorm2d(num_features))
+
+        # Linear layer
+        self.classifier = nn.Linear(num_features, num_classes)
+
+        # Official init from torch repo.
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        features = self.features(x)
+        out = F.relu(features, inplace=True)
+        out = F.adaptive_avg_pool2d(out, (1, 1)).view(features.size(0), -1)
+        out = self.classifier(out)
+        return out
 
 
 def densenet121(pretrained=False, **kwargs):
@@ -37,7 +80,6 @@ def densenet121(pretrained=False, **kwargs):
     """
     model = DenseNet(num_init_features=64, growth_rate=32, block_config=(6, 12, 24, 16),
                      **kwargs)
-    model.features[0] = nn.Conv2d(1, model.features[0].out_channels, kernel_size=7, stride=2, padding=3, bias=False)
     if pretrained:
         # '.'s are no longer allowed in module names, but pervious _DenseLayer
         # has keys 'norm.1', 'relu.1', 'conv.1', 'norm.2', 'relu.2', 'conv.2'.
