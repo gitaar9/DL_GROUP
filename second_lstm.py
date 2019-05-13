22bbbe502e223d3b80e34b742164af8302d3cc35
gitaar9 <thijs.eker@gmail.com>
2019-05-13 14:03:34 +0200

    try to fix the hidden layer not being on cuda

--- second_lstm.py
+++ second_lstm.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 
-from datasets import MidiClassicMusic
+from datasets import MidiClassicMusic, AUSLAN
 from networks import BaseNet
 
 
@@ -17,14 +17,15 @@ class RNN(nn.Module):
         self.num_layers = num_layers
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, dropout=.5, batch_first=True)
         self.fc = nn.Linear(hidden_size, num_classes)
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     def forward(self, x):
         # Put the input in the right order
         x = x.permute(0, 2, 1)
 
         # Set initial states
-        h0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size))
-        c0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size))
+        h0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(self.device))
+        c0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(self.device))
 
         # Forward propagate RNN
         out, _ = self.lstm(x, (h0, c0))
@@ -42,13 +43,23 @@ class OurLSTM(BaseNet):
         super().__init__(**kwargs)
 
     def get_data_loaders(self, train_batch_size, val_batch_size):
+        # train_loader = DataLoader(
+        #     MidiClassicMusic(folder_path="./data/midi_files_npy", train=True, slices=16, composers=self.composers, unsqueeze=False),
+        #     batch_size=train_batch_size,
+        #     shuffle=True
+        # )
+        # val_loader = DataLoader(
+        #     MidiClassicMusic(folder_path="./data/midi_files_npy", train=False, slices=16, composers=self.composers, unsqueeze=False),
+        #     batch_size=val_batch_size,
+        #     shuffle=False
+        # )
         train_loader = DataLoader(
-            MidiClassicMusic(folder_path="./data/midi_files_npy", train=True, slices=16, composers=self.composers, unsqueeze=False),
+            AUSLAN(train=True, unsqueeze=False),
             batch_size=train_batch_size,
             shuffle=True
         )
         val_loader = DataLoader(
-            MidiClassicMusic(folder_path="./data/midi_files_npy", train=False, slices=16, composers=self.composers, unsqueeze=False),
+            AUSLAN(train=False, unsqueeze=False),
             batch_size=val_batch_size,
             shuffle=False
         )
@@ -75,12 +86,13 @@ if __name__ == '__main__':
     composers = ['Brahms', 'Mozart', 'Schubert', 'Mendelsonn']  # , 'Haydn', 'Beethoven', 'Bach', 'Chopin']
     lstm = OurLSTM(
         composers=composers,
-        num_classes=4,
+        num_classes=96,
         epochs=100,
-        train_batch_size=100,
-        val_batch_size=100,
+        train_batch_size=1,
+        val_batch_size=1,
         num_layers=num_layers,
         hidden_size=hidden_size,
+        input_size=22,
         verbose=False
     )
     metrics = lstm.run()
