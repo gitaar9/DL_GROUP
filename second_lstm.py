24225893628a6034531440b3018b9e383d9d22e3
gitaar9 <thijs.eker@gmail.com>
2019-05-19 17:34:01 +0200

    adjusted the cnn and lstm scripts for further testing

--- second_lstm.py
+++ second_lstm.py
@@ -12,13 +12,14 @@ from networks import BaseNet
 
 # RNN Model (Many-to-One)
 class RNN(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes, dropout):
         super(RNN, self).__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
-        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, dropout=.5, batch_first=True)
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, dropout=dropout, batch_first=True)
         self.fc1 = nn.Linear(hidden_size, 256)
         self.fc2 = nn.Linear(256, num_classes)
+        self.dropout = dropout
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     def forward(self, x):
@@ -31,7 +32,7 @@ class RNN(nn.Module):
 
         # Forward propagate RNN
         x, _ = self.lstm(x, (h0, c0))
-        x = F.dropout(x, p=0.5, training=self.training)  # Dropout over the output of the lstm
+        x = F.dropout(x, p=self.dropout, training=self.training)  # Dropout over the output of the lstm
 
         # The output of the lstm for the last time step goes into the first fully connected layer
         x = self.fc1(x[:, -1, :])
@@ -43,9 +44,15 @@ class RNN(nn.Module):
 
 
 class OurLSTM(BaseNet):
-    def __init__(self, num_classes=10, input_size=72, hidden_size=8, num_layers=1, **kwargs):
+    def __init__(self, num_classes=10, input_size=72, hidden_size=8, num_layers=1, dropout=0.5, **kwargs):
         # load the model
-        self.model = RNN(num_classes=num_classes, input_size=input_size, num_layers=num_layers, hidden_size=hidden_size)
+        self.model = RNN(
+            num_classes=num_classes,
+            input_size=input_size,
+            num_layers=num_layers,
+            hidden_size=hidden_size,
+            dropout=dropout
+        )
 
         super().__init__(**kwargs)
 
@@ -71,26 +78,29 @@ def parse_arguments():
                         help='The lstm layers.')
     parser.add_argument('--hidden_size', type=int, default=8,
                         help='The amount of blocks in every lstm layer.')
+    parser.add_argument('--dropout', type=float, default=.5,
+                        help='The dropout rate after each lstm layer.')
 
     args = parser.parse_args()
 
-    return args.epochs, args.num_layers, args.hidden_size
+    return args.epochs, args.num_layers, args.hidden_size, args.dropout
 
 
 if __name__ == '__main__':
-    epochs, num_layers, hidden_size = parse_arguments()
+    epochs, num_layers, hidden_size, dropout = parse_arguments()
 
     composers = ['Brahms', 'Mozart', 'Schubert', 'Mendelsonn', 'Haydn', 'Beethoven', 'Bach', 'Chopin']
     lstm = OurLSTM(
         composers=composers,
         num_classes=len(composers),
-        epochs=100,
+        epochs=epochs,
         train_batch_size=100,
         val_batch_size=100,
         num_layers=num_layers,
         hidden_size=hidden_size,
+        dropout=dropout,
         verbose=False
     )
     metrics = lstm.run()
-    lstm.save_metrics("results/lstm_test2_{}_{}_{}".format(epochs, num_layers, hidden_size), metrics)
+    lstm.save_metrics("results/lstm_test3_{}_{}_{}_{}".format(epochs, num_layers, hidden_size, dropout), metrics)
 
--- simple_cnn.py
+++ simple_cnn.py
@@ -7,9 +7,9 @@ import torch.nn.functional as F
 from networks import BaseNet
 
 
-# RNN Model (Many-to-One)
+# Simple CNN implementation
 class SimpleCNN(nn.Module):
-    def __init__(self, num_classes, input_size=(72, 1600)):
+    def __init__(self, num_classes, use_batch_norm=True, dropout=0.4, input_size=(72, 1600)):
         super().__init__()
         self.conv1 = nn.Conv2d(1, 32, kernel_size=5, bias=False)
         self.bn1 = nn.BatchNorm2d(32)
@@ -24,33 +24,37 @@ class SimpleCNN(nn.Module):
         self.fc1 = nn.Linear(16 * after_maxpool_height * after_maxpool_width, 512)
         self.fc2 = nn.Linear(512, num_classes)
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.use_batch_norm = use_batch_norm
+        self.dropout = dropout
 
     def forward(self, x):
         x = self.conv1(x)
-        x = self.bn1(x)
+        if self.use_batch_norm:
+            x = self.bn1(x)
         x = F.relu(x)
         x = self.maxpool1(x)
 
         x = self.conv2(x)
-        x = self.bn2(x)
+        if self.use_batch_norm:
+            x = self.bn2(x)
         x = F.relu(x)
         x = self.maxpool2(x)
 
         x = x.view(x.size()[0], -1)
-        x = F.dropout(x, p=0.3, training=self.training)  # Dropout after second pooling layer 30%
+        x = F.dropout(x, p=self.dropout - 0.1 if self.dropout > 0.1 else 0, training=self.training)  # Dropout after second pooling layer 30%
 
         x = self.fc1(x)
         x = F.relu(x)
-        x = F.dropout(x, p=0.4, training=self.training)  # Dropout after first fully connected layer 40%
+        x = F.dropout(x, p=self.dropout, training=self.training)  # Dropout after first fully connected layer 40%
 
         x = self.fc2(x)
         return x
 
 
 class OurSimpleCNN(BaseNet):
-    def __init__(self, num_classes=10, **kwargs):
+    def __init__(self, num_classes=10, use_batch_norm=True, dropout=0.4, **kwargs):
         # load the model
-        self.model = SimpleCNN(num_classes=num_classes)
+        self.model = SimpleCNN(num_classes=num_classes, use_batch_norm=use_batch_norm, dropout=dropout)
 
         super().__init__(**kwargs)
 
@@ -59,24 +63,31 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description='Test some lstms on the midi database.')
     parser.add_argument('--epochs', type=int, default=100,
                         help='The amount of epochs that the model will be trained.')
+    parser.add_argument('--use_bn', default=False, action='store_true',
+                        help='When this argument is supplied batch normalization is used in the convolutional layers.')
+    parser.add_argument('--dropout', type=float, default=0.4,
+                        help='The dropout percentage used after the first fully-connected layer, this-0.1 is used as '
+                             'dropout percentage for the last conv layer.')
 
     args = parser.parse_args()
 
-    return args.epochs
+    return args.epochs, args.use_bn, args.dropout
 
 
 if __name__ == '__main__':
-    epochs = parse_arguments()
+    epochs, use_bn, dropout = parse_arguments()
 
     composers = ['Brahms', 'Mozart', 'Schubert', 'Mendelsonn', 'Haydn', 'Beethoven', 'Bach', 'Chopin']
-    lstm = OurSimpleCNN(
+    net = OurSimpleCNN(
         composers=composers,
         num_classes=len(composers),
-        epochs=100,
+        epochs=epochs,
         train_batch_size=100,
         val_batch_size=100,
-        verbose=False
+        verbose=False,
+        use_batch_norm=use_bn,
+        dropout=dropout
     )
-    metrics = lstm.run()
-    lstm.save_metrics("results/cnn_test2_{}".format(epochs), metrics)
+    metrics = net.run()
+    net.save_metrics("results/cnn_test3_{}_{}_{}".format(epochs, "bn" if use_bn else "", dropout), metrics)
 
