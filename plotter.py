bd95179af260518f0f4f29672180b0fdfb9601f2
gitaar9 <thijs.eker@gmail.com>
2019-05-19 15:49:14 +0200

    replaced the plotter added bn to the simple cnn + extra dropout

--- plotter.py
+++ plotter.py
@@ -6,14 +6,17 @@ import matplotlib.pyplot as plt
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Averages and plots output fom our pytorch networks.')
-    parser.add_argument('--amount_of_files', type=int, default=4,
+    parser.add_argument('--amount_of_files', type=int, default=1,
                         help='The amount of runs we did.')
-    parser.add_argument('--filename', type=str, default='default',
+    parser.add_argument('--filename', type=str, default=[], nargs='+',
                         help='The base filename of the files we want to read in.')
-    parser.add_argument('--plot_lstm_test_100', default=False, action='store_true')
+    parser.add_argument('--legend', type=str, default=[], nargs='+',
+                        help='The legend names of the filenames.')
+    parser.add_argument('--everything', default=False, action='store_true',
+                        help='Just plot all final results for the Deep Learning assignment')
 
     args = parser.parse_args()
-    return args.amount_of_files, args.filename, args.plot_lstm_test_100
+    return args.amount_of_files, args.filename, args.everything, args.legend
 
 
 def read_in_file(filename):
@@ -23,7 +26,9 @@ def read_in_file(filename):
 
 
 def read_in_files_to_average(filename, amount_of_files):
-    all_data = [read_in_file("{}_{}".format(filename, file_number)) for file_number in range(amount_of_files)]
+    all_data = [read_in_file("results/{}_{}".format(filename, file_number)) for file_number in range(amount_of_files)]
+    until_epoch = 400
+    all_data = list(map(lambda r: r[:until_epoch], all_data))
 
     # u = E(x) / n
     averages = sum(all_data) / amount_of_files
@@ -34,50 +39,72 @@ def read_in_files_to_average(filename, amount_of_files):
     return averages, stds
 
 
-def plot_collumn(data, collumn=5, ylabel='Accuracy'):
-    plt.plot(data[:, collumn])
-    plt.ylabel(ylabel)
+def plot_accuracy(averages, filename):
+    plt.plot(averages[:, 5], label=filename)
+    plt.ylabel('Accuracy')
     plt.xlabel('Epochs')
+    plt.legend(loc='bottom right')
     plt.show()
 
 
-def plot_multiple_file_data_arrays(list_of_averages, filenames, collumn=5, ylabel='Accuracy', loc='lower right'):
-    for averages in list_of_averages:
-        plt.plot(averages[:, collumn])
-    plt.ylabel(ylabel)
+def plot_losses(averages, filename):
+    plt.plot(averages[:, 0], label='test_loss'+filename)
+    plt.plot(averages[:, 1], label='validation_loss'+filename)
+    plt.ylabel('Loss')
     plt.xlabel('Epochs')
-    plt.legend(filenames, loc=loc)
+    plt.legend(loc='bottom right')
     plt.show()
 
 
-def plot_lstm_test_100():
-    layer_options = ['1', '2']
-    hidden_size_options = ['8', '16', '32', '64']
-    filenames = ["results/lstm_test2_100_{}_{}".format(l, h) for l in layer_options for h in hidden_size_options]
-    all_averages = [read_in_file(filename) for filename in filenames]
-    plot_multiple_file_data_arrays(all_averages, filenames, 5, 'Accuracy')
-    plot_multiple_file_data_arrays(all_averages, filenames, 0, 'Training loss', 'upper right')
-    plot_multiple_file_data_arrays(all_averages, filenames, 1, 'Validation loss', 'upper left')
+def plot_multiple_accuracies(list_of_averages, legend_names, colors=None):
+    colors = plt.rcParams['axes.prop_cycle'].by_key()['color'] if colors is None else colors
+    for idx, averages in enumerate(list_of_averages):
+        plt.plot(averages[:, 5], colors[idx])
+    plt.ylabel('Accuracy (%)')
+    plt.xlabel('Epochs')
+    # plt.axis([-1, 400, .65, 1])
+    plt.legend(legend_names, loc='lower right')
+    plt.show()
 
 
-def print_accuracies_and_stds(filenames):
+def plot_multiple_loss(list_of_averages, legend_names, colors=None):
+    colors = plt.rcParams['axes.prop_cycle'].by_key()['color'] if colors is None else colors
+    for idx, averages in enumerate(list_of_averages):
+        plt.plot(averages[:, 0], color=colors[idx], linestyle='--', label=legend_names[idx]+' test loss')
+        plt.plot(averages[:, 1], color=colors[idx], label=legend_names[idx]+' validation loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epochs')
+    # plt.axis([-1, 400, 0, 1.5])
+    plt.legend(loc='upper right')
+    plt.show()
+
+
+def print_everything(filenames):
     # Print final accuracies + stds
-    print("\t\t\t\ttraining_loss\tvalidation_loss\tprecision\trecall\t\tf1\t\taccuracy")
+    print("\t\t\ttraining_loss\tvalidation_loss\tprecision\trecall\t\tf1\t\taccuracy")
     averages_and_stds = [read_in_files_to_average(filename, 4) for filename in filenames]
     for filename, (averages, stds) in zip(filenames, averages_and_stds):
         s = filename + "\t" + ("\t" if "adam" in filename and "res_" in filename else "")
-        for average, std in zip(averages[-1,:], stds[-1,:]):
+        for average, std in zip(averages[-1, :], stds[-1, :]):
             s += "%0.3f+/-%0.3f\t" % (average, std)
         print(s)
 
 
-if __name__ == '__main__':
-    amount_of_files, filename, plot_lstm_test = parse_arguments()
-
-    if plot_lstm_test:
-        plot_lstm_test_100()
+def plot_selecting(filenames, amount_of_files, legend_names):
+    if amount_of_files > 1:
+        all_data = [read_in_files_to_average(each_file, amount_of_files)[0] for each_file in filenames]
     else:
-        averages = read_in_files_to_average(filename, amount_of_files)[0]
-        plot_collumn(averages, 5, 'Accuracy')
-        plot_collumn(averages, 0, 'Training loss')
-        plot_collumn(averages, 1, 'Validation loss')
+        all_data = [read_in_file(f) for f in filenames]
+    legend_names = list(map(lambda n: n.replace('_', '-'), legend_names))
+    plot_multiple_accuracies(all_data, legend_names if legend_names else filenames)
+    plot_multiple_loss(all_data, legend_names if legend_names else filenames)
+
+
+if __name__ == '__main__':
+    amount_of_files, filename, everything, legend_names = parse_arguments()
+
+    plot_selecting(filename, amount_of_files, legend_names)
+
+
+# python plotter.py --filename results/lstm_test2_100_2_8 results/lstm_test2_100_2_16 results/lstm_test2_100_2_32 results/lstm_test2_100_2_64
+# python plotter.py --filename results/lstm_test2_100_1_8 results/lstm_test2_100_1_16 results/lstm_test2_100_1_32 results/lstm_test2_100_1_64
--- simple_cnn.py
+++ simple_cnn.py
@@ -12,9 +12,13 @@ class SimpleCNN(nn.Module):
     def __init__(self, num_classes, input_size=(72, 1600)):
         super().__init__()
         self.conv1 = nn.Conv2d(1, 32, kernel_size=5, bias=False)
+        self.bn1 = nn.BatchNorm2d(32)
         self.maxpool1 = nn.MaxPool2d(2)
+
         self.conv2 = nn.Conv2d(32, 16, kernel_size=5, bias=False)
+        self.bn2 = nn.BatchNorm2d(16)
         self.maxpool2 = nn.MaxPool2d(2)
+
         after_maxpool_height = int((((input_size[0] - 4) / 2 - 4) / 2))
         after_maxpool_width = int((((input_size[1] - 4) / 2 - 4) / 2))
         self.fc1 = nn.Linear(16 * after_maxpool_height * after_maxpool_width, 512)
@@ -23,17 +27,21 @@ class SimpleCNN(nn.Module):
 
     def forward(self, x):
         x = self.conv1(x)
+        x = self.bn1(x)
         x = F.relu(x)
         x = self.maxpool1(x)
 
         x = self.conv2(x)
+        x = self.bn2(x)
         x = F.relu(x)
         x = self.maxpool2(x)
 
-        flattened_x = x.view(x.size()[0], -1)
-        x = self.fc1(flattened_x)
+        x = x.view(x.size()[0], -1)
+        x = F.dropout(x, p=0.3, training=self.training)  # Dropout after second pooling layer 30%
+
+        x = self.fc1(x)
         x = F.relu(x)
-        x = F.dropout(x, p=0.5, training=self.training)  # Dropout over the output of the lstm
+        x = F.dropout(x, p=0.4, training=self.training)  # Dropout after first fully connected layer 40%
 
         x = self.fc2(x)
         return x
