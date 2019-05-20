b5eb7d99c2f4eeff7232906ad94573cff8647bcd
gitaar9 <thijs.eker@gmail.com>
2019-05-20 09:39:17 +0200

    some more tests for the simple cnn

--- simple_cnn.py
+++ simple_cnn.py
@@ -9,19 +9,20 @@ from networks import BaseNet
 
 # Simple CNN implementation
 class SimpleCNN(nn.Module):
-    def __init__(self, num_classes, use_batch_norm=True, dropout=0.4, kernel_size=5, input_size=(72, 1600)):
+    def __init__(self, num_classes, use_batch_norm=True, dropout=0.4, kernel_size=5, filters=32, input_size=(72, 1600)):
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=kernel_size, bias=False)
-        self.bn1 = nn.BatchNorm2d(32)
+        self.conv1 = nn.Conv2d(1, filters, kernel_size=kernel_size, bias=False)
+        self.bn1 = nn.BatchNorm2d(filters)
         self.maxpool1 = nn.MaxPool2d(2)
 
-        self.conv2 = nn.Conv2d(32, 16, kernel_size=kernel_size, bias=False)
-        self.bn2 = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(32, int(filters / 2), kernel_size=kernel_size, bias=False)
+        self.bn2 = nn.BatchNorm2d(int(filters / 2))
         self.maxpool2 = nn.MaxPool2d(2)
 
-        after_maxpool_height = int((((input_size[0] - 4) / 2 - 4) / 2))
-        after_maxpool_width = int((((input_size[1] - 4) / 2 - 4) / 2))
-        self.fc1 = nn.Linear(16 * after_maxpool_height * after_maxpool_width, 512)
+        convolution_loss = int(kernel_size / 2) * 2
+        after_maxpool_height = int((((input_size[0] - convolution_loss) / 2 - convolution_loss) / 2))
+        after_maxpool_width = int((((input_size[1] - convolution_loss) / 2 - convolution_loss) / 2))
+        self.fc1 = nn.Linear(int(filters / 2) * after_maxpool_height * after_maxpool_width, 512)
         self.fc2 = nn.Linear(512, num_classes)
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.use_batch_norm = use_batch_norm
@@ -58,13 +59,14 @@ class SimpleCNN(nn.Module):
 
 
 class OurSimpleCNN(BaseNet):
-    def __init__(self, num_classes=10, use_batch_norm=True, dropout=0.4, kernel_size=5, **kwargs):
+    def __init__(self, num_classes=10, use_batch_norm=True, dropout=0.4, kernel_size=5, filters=32, **kwargs):
         # load the model
         self.model = SimpleCNN(
             num_classes=num_classes,
             use_batch_norm=use_batch_norm,
             dropout=dropout,
-            kernel_size=kernel_size
+            kernel_size=kernel_size,
+            filters=filters
         )
 
         super().__init__(**kwargs)
@@ -80,15 +82,17 @@ def parse_arguments():
                         help='The dropout percentage used after the first fully-connected layer, this-0.1 is used as '
                              'dropout percentage for the last conv layer.')
     parser.add_argument('--kernel_size', type=int, default=5,
-                        help='The amount of epochs that the model will be trained.')
+                        help='The size of the convolutional kernels.')
+    parser.add_argument('--filters', type=int, default=32,
+                        help='The amount of filters in the first convolutional layer, the second layer has this /2.')
 
     args = parser.parse_args()
 
-    return args.epochs, args.use_bn, args.dropout, args.kernel_size
+    return args.epochs, args.use_bn, args.dropout, args.kernel_size, args.filters
 
 
 if __name__ == '__main__':
-    epochs, use_bn, dropout, kernel_size = parse_arguments()
+    epochs, use_bn, dropout, kernel_size, filters = parse_arguments()
 
     composers = ['Brahms', 'Mozart', 'Schubert', 'Mendelsonn', 'Haydn', 'Beethoven', 'Bach', 'Chopin']
     net = OurSimpleCNN(
@@ -100,8 +104,10 @@ if __name__ == '__main__':
         verbose=False,
         use_batch_norm=use_bn,
         dropout=dropout,
-        kernel_size=kernel_size
+        kernel_size=kernel_size,
+        filters=filters
     )
     metrics = net.run()
-    net.save_metrics("results/cnn_test3_{}_{}_{}_{}".format(epochs, "bn" if use_bn else "", dropout, kernel_size), metrics)
+    filename = "results/cnn_test4_{}_{}_{}_{}_{}".format(epochs, "bn" if use_bn else "", dropout, kernel_size, filters)
+    net.save_metrics(filename, metrics)
 
