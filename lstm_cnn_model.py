dbbf1d958a1acc49908d66fd1a6cfe85f0affd32
Sohyo <cienakim12@gmail.com>
2019-05-30 16:17:27 +0200

    batch to 50

--- lstm_cnn_model.py
+++ lstm_cnn_model.py
@@ -119,7 +119,7 @@ if __name__ == '__main__':
         composers=composers,
         num_classes=len(composers),
         epochs=epochs,
-        batch_size=40,
+        batch_size=50,
         num_layers=num_layers,
         hidden_size=hidden_size,
         dropout=dropout,
