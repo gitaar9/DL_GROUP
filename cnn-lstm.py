4c79047ecb369fef57c292025360955765f2ccbf
Diego Cabo <dcgdiego@gmail.com>
2019-05-30 16:04:56 +0200

    Preliminar version for CNNLSTM, needs debugging

--- cnn-lstm.py
+++ cnn-lstm.py
@@ -6,6 +6,7 @@ import torchvision.models as models
 import torch.nn.functional as F
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
+from collections import OrderedDict
 
 from cross_validation_datasets import Mode, MidiClassicMusic
 from cross_validator import CrossValidator
@@ -16,29 +17,40 @@ import lstm
 import networks
 
 
-class CnnLstmModel(nn.Module):
-    def __init__(self, num_classes, input_size, cnn_pretrained, feature_extract,
+class SinglePassCnnLstmModel(nn.Module):
+    def __init__(self, cnn_pretrained, feature_extract,
                  lstm_input_size, lstm_hidden_size, num_lstm_layers, dropout):
         super().__init__()
-        self.lstm_hidden_size = lstm_hidden_size
-        self.num_lstm_layers = num_lstm_layers
-        self.dropout = dropout
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-        # CNN model
         self.cnn_model = self.build_cnn(cnn_pretrained, feature_extract, lstm_input_size)
+        # TODO: work to be done to fix the sequence dimension..
+        self.lstm_model = nn.LSTM(lstm_input_size, lstm_hidden_size, num_lstm_layers, dropout=dropout, batch_first=True)
 
-        # The LSTM layers
-        self.lstm = nn.LSTM(lstm_input_size, lstm_hidden_size, num_lstm_layers, dropout=dropout, batch_first=True)
-        self.add_module('lstm', self.lstm)
+        # self.classifier = nn.Linear(256, num_classes)
+        # self.classifier.add_module('fc2', nn.Linear(256, num_classes))
 
-        # Fully connected layer 1
-        self.fc1 = nn.Linear(lstm_hidden_size, 256)
-        self.add_module('fc1', self.fc1)
+        # # First convolution
+        # self.model = nn.Sequential(OrderedDict([
+        #     ('cnn', cnn_model),
+        #     ('lstm', nn.LSTM(lstm_input_size, lstm_hidden_size, num_lstm_layers, dropout=dropout, batch_first=True)),
+        #     # ('fc1', nn.Linear(lstm_hidden_size, 256)),
+        # ]))
+        # self.lstm_fc1 =nn.Linear(lstm_hidden_size, 256)
+        # self.classifier = nn.Linear(256, num_classes)
 
-        # Fully connected layer 2
-        self.fc2 = nn.Linear(256, num_classes)
-        self.add_module('fc2', self.fc2)
+    def forward(self, inputs):
+        inputs, (h_n, c_n) = inputs
+        cnn_output = self.cnn_model(inputs)
+        output, (h_n, c_n) = self.lstm_model(cnn_output, (h_n, c_n))
+        return (h_n, c_n)
+
+    # From densenet forward function:
+    # def forward(self, x):
+    #     features = self.features(x)
+    #     out = F.relu(features, inplace=True)
+    #     out = F.adaptive_avg_pool2d(out, (1, 1)).view(features.size(0), -1)
+    #     out = self.classifier(out)
+    #     return out
 
     def build_cnn(self, cnn_pretrained, feature_extract, lstm_input_size):
         # TODO: try building our own simple convolution layers (just a couple)
@@ -52,57 +64,39 @@ class CnnLstmModel(nn.Module):
         model.fc = nn.Linear(2048, lstm_input_size)  # 512 for resnet18, 2048 for resnet50
         return model
 
+
+class CnnLstmModel(nn.Module):
+    def __init__(self, num_classes, input_size, cnn_pretrained, feature_extract,
+                 lstm_input_size, lstm_hidden_size, num_lstm_layers, dropout):
+        super().__init__()
+        self.cnn_lstm = SinglePassCnnLstmModel(num_classes,
+                                               input_size,
+                                               cnn_pretrained,
+                                               feature_extract,
+                                               lstm_input_size,
+                                               lstm_hidden_size,
+                                               num_lstm_layers,
+                                               dropout)
+        self.add_module('cnn_lstm', self.cnn_lstm)
+        self.classifier = nn.Linear(hidden_size, 256)
+        self.classifier.add_module('fc2', nn.Linear(256, num_classes))
+        self.add_module('classifier', self.classifier)
+
     def forward(self, x):
-        # Put the input in the right order
-        # x = x.permute(0, 2, 1)  # TODO:needed here?
-        n_chunks = 20
-        list_x = torch.chunk(x, n_chunks, 0)  # TODO: dimension 0 or 1??
-
-        # Forward pass through ResNet # TODO: will this be backpropagated ok?
-        # Initialize sliced tensor.
-        output_cnn = torch.empty((n_chunks, x.size[0], x.size[1]), device=self.device, requires_grad=True)
-        # Fill in sliced tensor
-        for i, chunk in enumerate(list_x):
-            output_cnn[i] = self.cnn_model.forward(chunk)
-
-        # x = self.cnn_model.forward(x)
-        # x = self.cnn_model.conv1
-        # x = self.cnn_model.conv1(x)
-        # x = self.cnn_model.bn1(x)
-        # x = self.cnn_model.relu(x)
-        # x = self.cnn_model.maxpool(x)
-        #
-        # x = self.cnn_model.layer1(x)
-        # x = self.cnn_model.layer2(x)
-        # x = self.cnn_model.layer3(x)
-        # x = self.cnn_model.layer4(x)
-        #
-        # x = self.cnn_model.avgpool(x)
-        # x = x.view(x.size(0), -1)
-        # x = self.cnn_model.fc(x)
-
-        # Forward pass through LSTM
-
         # Set initial states <-- This might be unnecessary
-        h0 = Variable(torch.zeros(self.num_lstm_layers, x.size(0), self.lstm_hidden_size).to(self.device))
-        c0 = Variable(torch.zeros(self.num_lstm_layers, x.size(0), self.lstm_hidden_size).to(self.device))
+        h = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(self.device))
+        c = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(self.device))
 
-        output, (hn, cn) = self.lstm(x, (h0, c0))
-        x = F.dropout(x, p=self.dropout, training=self.training)  # Dropout over the output of the lstm
-
-        # The output of the lstm for the last time step goes into the first fully connected layer
-        # x = self.fc1(x[:, -1, :])
-        x = self.fc1(hn)
-        x = F.relu(x)
-
-        # Pass to the last fully connected layer (SoftMax)
-        x = self.fc2(x)
-        return x
+        n_chunks = 20
+        for chunk in torch.chunk(x, n_chunks, 0):
+            h, c = self.cnn_lstm((chunk, (h, c)))
+        return self.classifier(h)
 
 
 class OurCnnLstm(BaseNet):
-    def __init__(self, num_classes=10, input_size=72, cnn_pretrained=True, feature_extract=False,
-                 lstm_input_size = 72, lstm_hidden_size=8, num_lstm_layers=1, dropout=0.5, **kwargs):
+    def __init__(self, num_classes=10, input_size=72, cnn_pretrained=False,
+                 feature_extract=False, lstm_input_size=512, lstm_hidden_size=8,
+                 num_lstm_layers=1, dropout=0.5, **kwargs):
         # load the model
         self.model = CnnLstmModel(
             num_classes=num_classes,
@@ -147,28 +141,33 @@ class OurCnnLstm(BaseNet):
 
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='Test some lstms on the midi database.')
+    parser = argparse.ArgumentParser(description='Test different cnn-lstm models on the midi database.')
     parser.add_argument('--epochs', type=int, default=100,
                         help='The amount of epochs that the model will be trained.')
     parser.add_argument('--num_layers', type=int, default=1,
-                        help='The lstm layers.')
-    parser.add_argument('--hidden_size', type=int, default=8,
+                        help='The number of lstm layers.')
+    parser.add_argument('--hidden_size', type=int, default=512,
                         help='The amount of blocks in every lstm layer.')
     parser.add_argument('--dropout', type=float, default=.5,
                         help='The dropout rate after each lstm layer.')
-
+    # parser.add_argument('--cnn_pretrained', type=bool, default=False,
+    #                     help='If the CNN network uses pretrained weights.')
+    # parser.add_argument('--feature_extract', type=bool, default=False,
+    #                     help='If the CNN freezes the weights so no more training.')
+    parser.add_argument('--lstm_input_size', type=int, default=100,
+                        help='The output size of the CNN, as well as the input size of the LSTM.')
     args = parser.parse_args()
 
-    return args.epochs, args.num_layers, args.hidden_size, args.dropout
+    return args.epochs, args.num_layers, args.hidden_size, args.dropout, args.lstm_input_size
 
 
 if __name__ == '__main__':
-    epochs, num_layers, hidden_size, dropout = parse_arguments()
+    epochs, num_layers, hidden_size, dropout, lstm_input_size = parse_arguments()
 
     # composers = ['Brahms', 'Mozart', 'Schubert', 'Mendelsonn', 'Haydn', 'Beethoven', 'Bach', 'Chopin']
     composers = ['Brahms', 'Mozart', 'Schubert']
 
-    file_name = "lstm_test_precision8_{}_{}_{}_{}".format(epochs, num_layers, hidden_size, dropout)
+    file_name = "lstm_test_precision8_{}_{}_{}_{}_{}".format(epochs, num_layers, hidden_size, dropout, lstm_input_size)
 
     cv = CrossValidator(
         model_class=OurCnnLstm,
@@ -180,6 +179,7 @@ if __name__ == '__main__':
         num_layers=num_layers,
         hidden_size=hidden_size,
         dropout=dropout,
+        lstm_input_size=lstm_input_size,
         verbose=False
     )
 
